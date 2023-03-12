f0974f75c5894a83dd3a340487fd1f0acd399dd6
jhologic12 <jhonospino@gmail.com>
2023-03-11 21:39:33 -0500

    Subiendo archivos principales

---
+++ SignalRPrimeraAPP/Hubs/PosicionHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRPrimeraAPP.Hubs
+{
+    public class PosicionHub : Hub
+    {
+
+        public async Task EnviarPosicion(int left , int top)
+        {
+            await Clients.Others.SendAsync("RecibirPosicion", left, top);
+        }
+    }
+}
